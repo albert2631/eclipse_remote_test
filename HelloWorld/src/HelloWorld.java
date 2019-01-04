cb2495e40738478251f664b1fe6b241e6b9c3b0b
somiyuralove <jks2631@gmail.com>
2019-01-04 19:55:35 +0900

    2st master code change.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello World 7777");
+		System.out.println("Hello world 8888");
 	}
 }
