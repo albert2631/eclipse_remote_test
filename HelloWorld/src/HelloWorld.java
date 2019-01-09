d958f36ed2ea3870b02317c8f3e0fc2de194ed13
albert2631 <46235695+albert2631@users.noreply.github.com>
2019-01-09 20:18:02 +0900

    Update HelloWorld.java
    GitHub modify 8 line changed.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello World 7777");
 		System.out.println("Hello world 8888");
 		System.out.println("Hello World 9999");
+		System.out.println("GitHub modify 8 line");
 	}
 }
